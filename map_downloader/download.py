0e6afa933c908c737c84a116bf97f900b415c693
Thomas <thomas.kostas@yujinrobot.com>
2018-06-28 16:24:09 +0900

    Improvements
    - Version of requests api is now fixed.
    - Dslam file is now copied to map name.
    - When map does not provided site_id download is skipped.
    - When concert ip is not provided error mesage is displayed.

--- map_downloader/download.py
+++ map_downloader/download.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import socket
+import shutil
 
 
 def write_to_file(path, data):
@@ -16,22 +17,41 @@ def get_robot_uuid():
 
 class Conf_downloader():
     def __init__(self, worker_uuid):
-        print ("Attempting data download for " + worker_uuid)
-        self.base_url = 'http://192.168.10.241'
+        print ("Attempting to download data for " + worker_uuid)
+        self.hostname = 'concert'
+        self.base_url = 'http://' + self.hostname
+        self.show_target()
         self.uuid = worker_uuid
         self.worker_data = None
         self.configs = None
         self.default_map = None
+
+    def show_target(self):
+        try:
+            ip = socket.gethostbyname(self.hostname)
+        except socket.gaierror:
+            print("hostname : " + self.hostname +" cannot be resolved. Please ensure that the hostname and IP are"
+                                                 "correctly set in /etc/host file.")
+        print("Attempting to connect to " + self.base_url + " with ip: " + ip)
 
     def get_worker_data(self):
         url = self.base_url + ':10602/scheduler/v0/workers?uuid=' + self.uuid
         response = requests.get(url)
         self.worker_data = response.json()
         if len(self.worker_data) < 1:
-            print ("Worker with " + str(self.uuid) + " uuid not found on Server, exiting...")
+            print ("Worker with " + str(self.uuid) + " uuid not found on Server!")
+            self.generate_worker_on_concert()
             exit(1)
         else:
             self.worker_data = self.worker_data[0]
+
+    def generate_worker_on_concert(self):
+        print("Generating worker on concert server...")
+        username = self.uuid.split('@')[0]
+        requests.put('http://concert:10602/scheduler/v0/workers/',
+                     json={'uuid': self.uuid, 'name': username})
+        print("Worker successfully generated... Please configure your new robot on concert server"
+              "(fleet management UI > worker configuration)")
 
     def download_semantics(self, path=None, filename='semantics.yaml'):
         default_map = self.configs.get('default_map')
@@ -55,7 +75,7 @@ class Conf_downloader():
             print ("Worker with " + str(self.uuid) + " does not have configs on Server, exiting...")
             exit(2)
 
-    def download_task_definition(self, filename='tasks_definition.yaml'):
+    def download_task_definition(self, filename='task_definition.yaml'):
         url = self.base_url + ':10605/taskdefinition'
         tasks_definition_data = requests.post(url, json=self.configs.get('args_task_definition')).text
         home = os.path.expanduser("~")
@@ -93,6 +113,27 @@ class Conf_downloader():
             print map.get('map_data').get('nvmap_path')
             print map.get('map_data').get('metadata_path')
 
+    def verify_map(self, map):
+        if map.get("site_id") is None:
+            return False
+        else:
+            return True
+
+    def copy_dslam_file_to_map_name(self, map_name):
+        home = os.path.expanduser("~")
+        map_folder_path = home + '/' + '.ros/' + 'gopher/' + 'maps/'
+        file_list = os.listdir(map_folder_path)
+        for file in file_list:
+            if file.split('.')[-1] == 'dslam':
+                to_copy_path = map_folder_path + file
+                dest_path = map_folder_path + map_name + ".dslam"
+                try:
+                    shutil.copy(to_copy_path, dest_path)
+                except shutil.Error:
+                    pass
+                return
+        print("No .dslam file has been found under " + map_folder_path + " please add one and retry")
+
     def download_maps(self):
         map_list_url = self.base_url + ':10605/configuration/maps' #':10605/public/' #
         map_list = requests.get(map_list_url).json()
@@ -112,27 +153,32 @@ class Conf_downloader():
 
             file_path = dest_folder + map.get('uuid')
 
-            saved = []
-            if img_path is not None:
-                map_img = requests.get(base_dl_url + img_path)
-                write_to_file(file_path + '.png', map_img.content)
-                saved.append(file_path + '.png')
+            if self.verify_map(map) is True:
+                saved = []
+                self.copy_dslam_file_to_map_name(map.get('uuid'))
+                if img_path is not None:
+                    map_img = requests.get(base_dl_url + img_path)
+                    write_to_file(file_path + '.png', map_img.content)
+                    saved.append(file_path + '.png')
 
-            if nvmap_path is not None:
-                nvmap = requests.get(base_dl_url + nvmap_path)
-                write_to_file(file_path + '.nvmap', nvmap.content)
-                saved.append(file_path + '.nvmap')
+                if nvmap_path is not None:
+                    nvmap = requests.get(base_dl_url + nvmap_path)
+                    write_to_file(file_path + '.nvmap', nvmap.content)
+                    saved.append(file_path + '.nvmap')
 
-            if metadata_path is not None:
-                metadata = requests.get(base_dl_url + metadata_path)
-                write_to_file(file_path + '.grid', metadata.content)
-                saved.append(file_path + '.grid')
+                if metadata_path is not None:
+                    metadata = requests.get(base_dl_url + metadata_path)
+                    write_to_file(file_path + '.grid', metadata.content)
+                    saved.append(file_path + '.grid')
 
-            for file_saved in saved:
-                print ("Map related file successfully saved under " + file_saved)
+                for file_saved in saved:
+                    print ("Map related file successfully saved under " + file_saved)
+            else:
+                print('Map with uuid: ' + map.get('uuid') + ' won\'t be downloaded as it does not define'
+                                                            'a site_id')
 
     def process(self):
-        self.get_worker_data()
+        print self.get_worker_data()
         self.load_n_check_configs()
         self.download_semantics()
         self.download_task_definition()
