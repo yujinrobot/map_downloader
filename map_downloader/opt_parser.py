477c50e493d468ffacb855afaeab11b6c3082d0f
Thomas <thomas.kostas@yujinrobot.com>
2018-09-18 14:17:39 +0900

    Adding missing opt_parser file
    now opt_paser code file is part of the commited code...

---
+++ map_downloader/opt_parser.py
@@ -0,0 +1,15 @@
+import argparse
+from download import Conf_downloader, get_robot_uuid
+
+def pars_args():
+    parser = argparse.ArgumentParser(description="A simple tool to download robot configuration", prog='map_downloader',
+                                     version='0.11')
+    parser.add_argument("--skip_map", action="store_true", default=False, help="Skip map downloading but keep "
+                                                                               "downloading semantics, task definition,"
+                                                                               "and AR markers data." )
+    return parser.parse_args()
+
+def main():
+    parsed_args = pars_args()
+    dwl = Conf_downloader(get_robot_uuid(), skip_map=parsed_args.skip_map)
+    dwl.process()
